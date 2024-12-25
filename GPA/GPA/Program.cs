6b2ae2ba4576cb1c4100a1ceefc4bc87dbf22007
Buse Yılmaz <bsylm@outlook.com>
2024-12-25 21:51:09 +0300

    GPA
    An application that calculates the grade point average based on the user's inputs and provides information on whether they have passed or failed.

---
+++ GPA/GPA/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gpa();
+
+            Console.Read();
+        }
+
+        static void Gpa()
+        {
+            try
+            {
+                int total = 0;
+                Console.Write("Please enter the number of how many lessons you have in total:");
+
+                int lesson = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < lesson; i++)
+                {
+                    Console.Write((i + 1) + ". enter the lesson:");
+                    int grade = Convert.ToInt32(Console.ReadLine());
+
+                    if (grade>100 || grade<0)
+                    {
+                        Console.WriteLine("Error");
+                        Console.Write((i + 1) + ". enter the lesson:");
+                        grade = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    total += grade;
+                }
+
+                int average = total / lesson;
+
+                string result = (average > 60) ? "You Passed the Class" : "You Failed the Class";
+
+                Console.Write("Your result:" + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex);
+            }
+
+
+        }
+    }
+}
