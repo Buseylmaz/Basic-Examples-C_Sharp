a1ec86e39859775334f5b32f3e58cd38f09aa239
Buse Yılmaz <bsylm@outlook.com>
2024-12-25 23:22:50 +0300

    The Program that Finds the Hypotenuse

---
+++ The Program that Finds the Hypotenuse/The Program that Finds the Hypotenuse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace The_Program_that_Finds_the_Hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first edge: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second edge: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the third edge: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Hypotenuse(a, b);
+            TriangleArea(a, b, c);
+
+            Console.Read();
+        }
+
+        static int Hypotenuse(int edge1, int edge2)
+        {
+            int edge3 = (edge1 * edge1) + (edge2 * edge2);
+            double a = Math.Sqrt(edge3);
+            Console.WriteLine("Hypotenuse edge:" + a);
+            return edge3;
+        }
+
+        static int TriangleArea(int edge1, int edge2, int edge3)
+        {
+            int u = (edge1 + edge2 + edge3) / 2;
+            int area = (u * (u - edge1) * (u - edge2) * (u - edge3));
+
+            Console.WriteLine("Triangle area:" + area);
+            return area;
+
+
+        }
+    }
+}
