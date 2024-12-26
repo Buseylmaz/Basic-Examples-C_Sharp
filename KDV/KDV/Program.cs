2abd4e026a7da25aa9c5d7cafbbbc35bfea415da
Buse Yılmaz <bsylm@outlook.com>
2024-12-26 11:14:57 +0300

    KDV
    A program that calculates the KDV-based price and KDV amount of the value of money received from the user and prints it on the screen.

---
+++ KDV/KDV/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Amount of Money:");
+            double amount_money = Convert.ToDouble(Console.ReadLine());
+
+            if (amount_money < 0)
+            {
+                Console.WriteLine("The Amount of Money cannot be less than 0.");
+            }
+
+            double kdv = DetermineTheKDVRate(amount_money);
+            double calculate_money = CalculateMoney(amount_money, kdv);
+
+
+            Console.Write("Your Amount of Money:" + calculate_money);
+            Console.Read();
+        }
+
+        static double DetermineTheKDVRate(double money)
+        {
+            return money < 1000 ? 0.18 : 0.08;
+        }
+
+        static double CalculateMoney(double money, double kdv)
+        {
+            return (money * kdv) + money;
+        }
+    }
+}
