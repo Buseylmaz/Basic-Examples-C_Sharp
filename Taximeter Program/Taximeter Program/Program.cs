c8bb2ca74d7e5bbb11eb44084b220f396a0ab4ba
Buse Yılmaz <bsylm@outlook.com>
2024-12-26 12:06:46 +0300

    Taximeter Program
    The application that writes the taximeter price according to the route taken.

---
+++ Taximeter Program/Taximeter Program/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taximeter_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Road();
+            RoadToll(r);
+
+            Console.Read();
+
+        }
+
+        static double Road()
+        {
+            Console.Write("How many km did you travel?:");
+            double roadLength = Convert.ToDouble(Console.ReadLine());
+
+            return roadLength;
+        }
+
+        static double RoadToll(double roadLength)
+        {
+            int beginning = 10;
+            int minFee = 20;
+            double kmFee = 2.20;
+
+            double roadToll = (roadLength * kmFee) + beginning;
+
+            if (roadToll < minFee)
+            {
+                roadToll = minFee;
+            }
+
+            Console.Write("Road toll" + roadToll);
+
+
+            return roadToll;
+
+
+        }
+    }
+}
