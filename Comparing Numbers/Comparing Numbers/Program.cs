9f04bc148d1e29f589f345fca96acaaff255839b
Buse Yılmaz <bsylm@outlook.com>
2024-12-26 11:50:23 +0300

    Comparing Numbers
    A program that sorts numbers from large to small.

---
+++ Comparing Numbers/Comparing Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Comparing_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers();
+
+            Console.ReadLine();
+        }
+
+        static void Numbers()
+        {
+            Console.Write("How many numbers will be entered?:");
+            int numberArray = Convert.ToInt32(Console.ReadLine());
+
+            int[] number = new int[numberArray];
+
+            for (int i = 0; i < numberArray; i++)
+            {
+                Console.Write("Enter a number:");
+                int s = Convert.ToInt32(Console.ReadLine());
+                number[i] = s;
+            }
+
+            Array.Sort(number);
+
+            Array.Reverse(number);
+
+            foreach (var n in number)
+            {
+                Console.Write(n + "-");
+            }
+
+
+
+        }
+    }
+}
